c3719cb2f84ee8492eb01cfa95521c340cc8661b
rwbfd <ran.wang.math@gmail.com>
2021-04-27 17:33:30 +0800

    Finished chapter 13

--- chap02/target_encoding/target_encoding_v1.py
+++ chap02/target_encoding/target_encoding_v1.py
@@ -6,7 +6,7 @@ import pandas as pd
 def target_mean_v1(data, y_name, x_name):
     result = np.zeros(data.shape[0])
     for i in range(data.shape[0]):
-        groupby_result = data[data.index != i].groupby([x_name], as_index=False).agg(['mean', 'count'])
+        groupby_result = data[data.index != i].groupby([x_name], as_index=False).agg(['mean'])
         result[i] = groupby_result.loc[groupby_result.index == data.loc[i, x_name], (y_name, 'mean')]
     return result
 
